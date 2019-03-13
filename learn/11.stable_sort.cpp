8cce628635364e2b7851a2f1950f310a1f9c1caf
joey-z <865133107@qq.com>
2019-03-14 01:18:10 +0800

    添加

--- learn/11.stable_sort.cpp
+++ learn/11.stable_sort.cpp
@@ -36,7 +36,7 @@ void insert_sort(int *num, int n) {
 
 void bubble_sort(int *num, int n) {
     int times = 1;
-    for  (int i = 1; i < n; i++) {
+    for  (int i = 1; i < n & times; i++) {//加入了优化
         times = 0;
         for (int j = 0; j < n; j++) {
             if (num[j] <=  num[j + 1]) continue;  
@@ -57,15 +57,23 @@ void merge_sort(int *num, int l, int r) {
     int mid = (l + r) >> 1;
     merge_sort(num, l, mid);
     merge_sort(num, mid + 1, r);
+    //归并过程需要一段新的存储区
     int *temp = (int *)malloc(sizeof(int) * (r - l + 1));
     int p1 = l, p2 = mid + 1, k = 0;
-    while (p1 <= mid || p2 <= r) {
+    //第一个数组的头部元素　第二个数组的头部元素　
+    //临时存储区的第一个位置（当前可以存储的位置）
+    
+    while (p1 <= mid || p2 <= r) {//第一个数组有元素或者第二个数组有元素
+    //继续进行归并操作
+    //将第一个数组的元素放入临时存储区的条件：
+    //第二个数组为空或者第一个数组不为空并且第一个数组的当前元素小于等于第二个数组中的当前元素
         if (p2 > r || p1 <= mid && num[p1] <= num[p2]) {
             temp[k++] = num[p1++];
         } else {
-            temp[k++] = num[p2++];
+            temp[k++] = num[p2++];//将第二个数组的元素放入临时存储区
         }
     }
+    //将临时存储区的数据拷贝到num数组中
     memcpy(num + l, temp, sizeof(int) * (r - l + 1));
     free(temp);
     return ;
